74fdfaaa3e81aa58602db9bc728ae2a6f783099c
Parth2412 <parthnpatil06@gmail.com>
2021-02-03 20:58:08 +0530

    Added location name to Home screen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { StyleSheet, View, Text,  ScrollView} from 'react-native';
 import { Modal , Portal } from "react-native-paper";
+import Dash from 'react-native-dash';
+import Icon from "react-native-vector-icons/MaterialIcons"
 import data from "../api/data";
 import Category from "./Category"
 import SearchBar from "./SearchBar";
@@ -12,12 +14,21 @@ function HomeScreen() {
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
     return (
-        <View style={{flex : 1}}>
+        <View style={{flex : 1 , marginTop : 15}}>
             <Portal>
                 <Modal style={{ justifyContent : "center" }} visible={visible}  onDismiss={hideModal}> 
                     <Map latitude={latitude} longitude={longitude}/>
                 </Modal>
             </Portal>
+            <View style={{flexDirection : "row" , alignItems : "center"}}>
+                <Icon name="location-pin" size = {20} style={{marginHorizontal : 7}}/>
+                <View>
+                    <Text>
+                        Home - Narayan Nagar
+                    </Text>
+                    <Dash dashThickness={1} />
+                </View>
+            </View>
             <SearchBar/>
             <ScrollView>
                 <View style={[styles.container , {marginLeft : 10}]}>
