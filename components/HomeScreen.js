f65dd3c94a180742657f9cbe705cd38c34a120b4
Parth2412 <parthnpatil06@gmail.com>
2021-02-04 00:19:08 +0530

    Added API integration

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text,  ScrollView} from 'react-native';
 import { Modal , Portal } from "react-native-paper";
 import Dash from 'react-native-dash';
 import Icon from "react-native-vector-icons/MaterialIcons"
-import data from "../api/data";
 import Category from "./Category"
 import SearchBar from "./SearchBar";
 import Map from "./Map";
 
 function HomeScreen() {
     const [visible, setVisible] = useState(false);
+    const [data, setData] = useState({});
     const [[latitude,longitude] , setLocation] = useState([null, null])
+
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
+
+    useEffect(() => {
+        fetch("https://run.mocky.io/v3/2b067794-7df5-4b1a-8ea2-c30309349c2b")
+        .then(response => response.json())
+        .then(result => setData(result))
+        setData()
+    }, [])
     return (
         <View style={{flex : 1 , marginTop : 15}}>
             <Portal>
@@ -33,8 +41,8 @@ function HomeScreen() {
             <ScrollView>
                 <View style={[styles.container , {marginLeft : 10}]}>
                     {
-                        Object.keys(data).map(category => (
-                            <Category key={category} setLocation={setLocation} openMap={showModal} category={category}/>
+                        data && Object.entries(data).map(([category , services])  => (
+                            <Category key={category} setLocation={setLocation} openMap={showModal} category={category} services={services}/>
                         ))
                     }
                 </View>
