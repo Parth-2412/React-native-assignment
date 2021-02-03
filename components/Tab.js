43d0296b837709d35496f79a03d1a23618e6c6f7
Parth2412 <parthnpatil06@gmail.com>
2021-02-03 21:55:16 +0530

    Added other tabs

--- components/Tab.js
+++ components/Tab.js
@@ -2,22 +2,39 @@ import React , { useState } from 'react';
 import {  Dimensions , Text, View} from 'react-native';
 import { TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import Icon from "react-native-vector-icons/FontAwesome5";
+import MUIcon from "react-native-vector-icons/MaterialIcons"
+import FAIcon from "react-native-vector-icons/FontAwesome"
 import HomeScreen from "./HomeScreen";
-import DirectoryScreen from "./DirectoryScreen"
+import OtherScreen from "./OtherScreen"
 
 const initialLayout = { width: Dimensions.get('window').width };
 
-const getTabButtonIconName = (key,focused) => {
-    let name;
+const TicketScreen = () => <OtherScreen text="Ticket"/>
+const DirectoryScreen = () => <OtherScreen text="Directory"/>
+const FavouriteScreen = () => <OtherScreen text="Favourite"/>
+const MoreScreen = () => <OtherScreen text="More"/>
+
+const getTabButtonIcon = (key,focused) => {
+    const color = focused ? "white" : "#d9d9d7"
+    let icon;
     switch(key){
         case "home":
-            name = "home"
+            icon = <Icon name = "home" size={45} color={color} /> 
             break;
         case "directory":
-            name = "folder"
+            icon = <Icon name = "folder" size={45} color={color} /> 
+            break;
+        case "ticket":
+            icon = <FAIcon name = "ticket" size={45} color={color} /> 
+            break;
+        case "favourite":
+            icon = <Icon name = "heart" size={45} color={color} /> 
+            break;
+        case "more":
+            icon = <MUIcon name="menu" size = {45} color={color}/>
             break;
     }
-    return name;
+    return icon;
 }
 function Tab() {
     const [index, setIndex] = useState(0);
@@ -30,10 +47,25 @@ function Tab() {
             key : "directory",
             title : "Directory"
         },
+        {
+            key : "ticket",
+            title : "Ticket"
+        },
+        {
+            key : "favourite",
+            title : "Favourite"
+        },
+        {
+            key : "more",
+            title : "More"
+        },
     ])
     const renderScene = SceneMap({
         directory : DirectoryScreen,
-        home : HomeScreen 
+        ticket : TicketScreen,
+        home : HomeScreen,
+        favourite : FavouriteScreen,
+        more : MoreScreen
       });
     return (
         <TabView
@@ -44,7 +76,8 @@ function Tab() {
                     indicatorStyle={{ backgroundColor: 'black', height : 5 }}
                     renderLabel={ ({route , focused}) => (
                         <View style={{alignItems: "center"}}>
-                            <Icon name = {getTabButtonIconName(route.key, focused)} size={45} color="white" />
+                            {getTabButtonIcon(route.key, focused)}
+                            {/* <Icon name = {getTabButtonIconName(route.key, focused)} size={45} color="white" /> */}
                             <Text style={{fontSize: 10 , color : "white"}}>{route.title}</Text>
                         </View>
                     )}
